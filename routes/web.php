c3614e133d2dd8b495c575827dfdd201e7706f5e
Maggzzi <2217547@talnet.nl>
2025-09-12 12:43:20 +0200

    create get route voor planeten

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// get is een request naar een server doet
+// planeten is een url
+Route::get('/planeten', function () {
+    $myPlanets = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return $myPlanets;
+});
