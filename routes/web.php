36ace7e4d15d2a83f08bedd42c35f67144fa2b41
Maggzzi <2217547@talnet.nl>
2025-09-19 12:57:53 +0200

    Added view planeten

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,27 @@ Route::get('/', function () {
 // get is een request naar een server doet
 // planeten is een url
 Route::get('/planeten', function () {
-    $myPlanets = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    return $myPlanets;
+    //$myPlanets = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    
+
+    $planets = [
+    [
+        'name' => 'Mars',
+        'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+    ],
+    [
+        'name' => 'Venus',
+        'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+    ],
+    [
+        'name' => 'Earth',
+        'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far thats inhabited by living things.'
+    ],
+    [
+        'name' => 'Jupiter',
+        'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+    ]
+];
+
+return view("planeten", ['planets' => $planets]);
 });
