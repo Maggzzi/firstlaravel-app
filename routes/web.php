5f792a23be21cb6bdbd64a155afec0d937cb386c
Maggzzi <2217547@talnet.nl>
2025-09-30 20:04:58 +0200

    collection toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PlanetsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// get is een request naar een server doet
+// get is een request dat naar een server doet
 // planeten is een url
-Route::get('/planeten', function () {
-    //$myPlanets = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
-    
+// Route::get('/planets', [PlanetsController::class, 'index']);
 
-    $planets = [
+
+Route::get('/planeten', function () {
+
+    $planeten = [
     [
         'name' => 'Mars',
         'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
@@ -31,5 +33,22 @@ Route::get('/planeten', function () {
     ]
 ];
 
-return view("planeten", ['planets' => $planets]);
+    // maak collection van planets (en niet voor elke array hardcoded schrijven)
+    $collection = collect($planeten);
+
+    // wordt gecheckt of GET-parameter al bestaat door (request()->has())
+    if (request()->has('planeet')) {
+        $search = request('planeet');
+        // filtreet nu op name
+        $collection = $collection->where('name', ucfirst(strtolower($search)));
+        
+    }
+
+    return view("planeten", ['planeten' => $collection->all()]);
 });
+
+
+
+
+
+
