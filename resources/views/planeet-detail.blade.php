df2a4071284a1fccb7c7f03efd7345c00b46ce9b
Maggzzi <2217547@talnet.nl>
2025-10-03 22:51:30 +0200

    nieuwe planeet-detail.blade.php file (view) toegevoegd

---
+++ resources/views/planeet-detail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $planeet['name'] }}</title>
+</head>
+<body>
+    <h1>{{ $planeet['name'] }}</h1>
+    <p>{{ $planeet['description'] }}</p>
+    
+</body>
+</html>
